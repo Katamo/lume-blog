85d43e003864bfd5de58310482e043823e39f3c2
Óscar de la Mata <oscar@hanzo.es>
2022-09-15 16:00:50 +0200

    use custom postcss file

--- _config.ts
+++ _config.ts
@@ -6,8 +6,10 @@ import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
-import netlifyCMS from "lume/plugins/netlify_cms.ts";
 import gpm from "https://deno.land/x/gpm@v0.4.1/mod.ts";
+import postcssConvertUnits from "./js/vendor/postcss-convert-units/index.js"
+// import postcssFontFormatKeywords from 'https://github.com/Katamo/postcss-convert-units/blob/main/mod.js'
+
 
 const site = lume({
   location: new URL("https://example.com/"),
@@ -16,14 +18,28 @@ const site = lume({
 site
   .ignore("README.md")
   .copy("img")
-  .use(postcss())
+  .use(
+    postcss(
+      {
+        plugins: [
+          postcssConvertUnits({
+            units: [
+              {
+                from: 'sp',
+                convert: val => `${val * 0.25}rem`
+              }
+            ],
+          })
+        ],
+      }
+    )
+  )
   .use(terser())
   .use(date())
   .use(codeHighlight())
   .use(basePath())
   .use(slugifyUrls({ alphanumeric: false }))
   .use(resolveUrls())
-  .use(netlifyCMS({ netlifyIdentity: true }))
   .addEventListener(
     "beforeBuild",
     () => gpm(["oom-components/searcher"], "js/vendor"),
